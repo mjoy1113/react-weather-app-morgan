13810c23fae36e4c4c678a9027f03cba292016cb
mjoy1113 <mjoy1113@gmail.com>
2022-12-13 17:57:16 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Morgan Rumsey and is open-sourced on{" "}
-        <a
-          href="https://github.com/mjoy1113/react-weather-app-morgan.git"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Morgan Rumsey and is open-sourced on{" "}
+          <a
+            href="https://github.com/mjoy1113/react-weather-app-morgan.git"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
